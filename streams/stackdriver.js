0ab7301084aaf05aca325047bf94634087a47f3b
Nathan Cobb <nathancobb@gmail.com>
2019-10-08 14:45:41 -0700

    removed stackdriver key log

--- streams/stackdriver.js
+++ streams/stackdriver.js
@@ -1,7 +1,7 @@
 const { LoggingBunyan, LOGGING_TRACE_KEY } = require("@google-cloud/logging-bunyan");
 
 const stackdriver = ({ logLevel, logName, version }) => {
-    console.log("LOGGING_TRACE_KEY", LOGGING_TRACE_KEY);
+    // console.log("LOGGING_TRACE_KEY", LOGGING_TRACE_KEY);
     return new LoggingBunyan({
         serviceContext: {
             service: logName,
