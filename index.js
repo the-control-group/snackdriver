4a9de5b93efb5d8b6cb9f9c8636828a6cd3bcec2
Nathan Cobb <nathancobb@gmail.com>
2019-10-28 11:19:25 -0700

    broke out req, res, ctx to separate logs

--- index.js
+++ index.js
@@ -72,7 +72,9 @@ class Logger extends Bunyan {
 
             response.responseTime = seconds * 1e3 + nanoseconds * 1e-6;
 
-            ctx.log.info({ request, response, ctx }, msg);
+            ctx.log.info({ request }, msg);
+            ctx.log.info({ response}, msg);
+            ctx.log.info({ ctx }, msg);
         };
     }
 
